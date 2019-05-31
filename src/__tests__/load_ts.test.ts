65000d1832e709f7ec724376ef0c661571db5b4b
Ben <ben@zfben.com>
2019-05-31 19:53:18 +0800

    fix loadTs

--- src/__tests__/load_ts.test.ts
+++ src/__tests__/load_ts.test.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process';
 
 describe('loadTs', function () {
   test('should work', async function () {
-    const version = execSync(`yarn list @faasjs/deep_merge`).toString().match(/([0-9a-z.-]+)\n/)![1];
+    const version = execSync(`yarn list @faasjs/deep_merge`).toString().match(/@([0-9a-z.-]+)[^@]*\n/)![1];
 
     expect(await loadTs(require.resolve('./base.ts'), {
       tmp: true
--- src/load_ts.ts
+++ src/load_ts.ts
@@ -43,7 +43,7 @@ export default async function loadTs (filename: string, options: {
   for (const m of bundle.cache.modules || []) {
     for (const d of m.dependencies) {
       if (!d.startsWith('/') && !dependencies[d as string]) {
-        dependencies[d as string] = execSync(`yarn list ${d}`).toString().match(/([0-9a-z.-]+)\n/)![1];
+        dependencies[d as string] = execSync(`yarn list ${d}`).toString().match(/@([0-9a-z.-]+)[^@]*\n/)![1];
       }
     }
   }
